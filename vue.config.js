aaed4cb36b5d2b37d40bd309e1848160e088ce22
Thomas CASTELLY <tcastelly@corepetrol.com>
2018-02-15 08:44:39 +0100

    Update dependencies

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
   lintOnSave: true,
   configureWebpack: config => {
     if(process.argv[process.argv.length - 1] === 'cookbook') {
-      config.entry.app = ['./src/cookbook.js'];
+      config.entry.app = './src/cookbook.js';
     }
-    return config;
   }
 };
